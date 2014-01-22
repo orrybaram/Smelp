bb6309d4511f48d1da2a8d3bd6fc9a45d66d227d
Orry Baram <orrybaram@gmail.com>
2014-01-21 23:31:45 -0500

    fixed styling

--- smelp.js
+++ smelp.js
@@ -1,48 +1,51 @@
 var TESTING = true;
-
-
 var ROOT_URL = "http://netflixrottenizer.appspot.com/yelp/";
 if (TESTING) ROOT_URL = "http://localhost:11090/yelp";
 
-
+var new_stuff_added = false;
 
 $(function() {
     
-
     // ----------------------- MAIN RESTAURANT LIST ----------------------- //
     if($('#RestaurantResults').length) {
-
-        var $vendorList = $('#VendorsTable');
+        
         
         // Get the location by scraping the source code
         var search_zip = $('body').html().match("addressJs.preloadedZip \= '(.*)\'\;")[1];
 
-        $vendorList.on('DOMSubtreeModified', function(ok) {
-            console.log('stuff added');
-        });
-        
-        $vendorList.find('tr').each(function() {
-            var $this = $(this);
-            var restaurant = $this.find('h3 .tooltip-header').text().replace(/[^\w\s]/gi, ''); // get rid of special characters
-            
-            var $ratingCell = $this.find('td:nth-child(4)')
+        function getYelpRatingsForList() {
+            var $vendorList = $('#VendorsTable');
+            $vendorList.find('tr').each(function() {
+                var $this = $(this);
+                if(!$this.hasClass('smelped')) {
+                    var restaurant = $this.find('h3 .tooltip-header').text().replace(/[^\w\s]/gi, ''); // get rid of special characters
+                
+                    var $ratingCell = $this.find('td:nth-child(4)')
 
-            var url = ROOT_URL + '?term=' + restaurant + '&location=' + search_zip + '&limit=1'
-            $.getJSON(url).
-                success(function(data) {
-                    if(!data.error && data.businesses.length) {
-                        var smelp_rating = createSmelpRating(data.businesses[0])
-                        $ratingCell.append(smelp_rating);   
-                        $('.step2LocationRating').hide(); 
-                        $('.rating-count').hide();
-                    }
-                })
-            ;
-        });
+                    var url = ROOT_URL + '?term=' + restaurant + '&location=' + search_zip + '&limit=1'
+                    $.getJSON(url).
+                        success(function(data) {
+                            if(!data.error && data.businesses.length) {
+                                var $smelp_rating = createSmelpRating(data.businesses[0])
+                                if($this.find('.smelp-rating').length === 0) {
+                                    $ratingCell.prepend($smelp_rating).addClass('ratings-got-smelped');
+                                    $smelp_rating.fadeIn();
+                                }
+                            }
+                            $this.addClass('smelped');
+                        })
+                    ;
+                }
+            });
+            setTimeout(function() { getYelpRatingsForList() }, 1000);    
+        }
+        getYelpRatingsForList();
+
     }
 
 
     // ----------------------- RESTAURANT DETAIL PAGE ----------------------- //
+    
     if($('#RestaurantPage').length) {
         var restaurant_name = $('#VendorName').text();
         var restaurant_zip = $('[itemprop="postalCode"]').text();
@@ -53,12 +56,11 @@ $(function() {
                 if(!data.error && data.businesses.length) {
                     console.log(data);
                     var smelp_rating = createSmelpRating(data.businesses[0])
-                    $('#RatingBox').find('ul').append(smelp_rating);
+                    $('#PriceRange').before(smelp_rating);
                 }
             })
         ;
     }
-
 });
 
 function createSmelpRating(restaurant) {
