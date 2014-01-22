f8a23f213208d1fd855d931967649a03f4d975d4
Orry Baram <orrybaram@gmail.com>
2014-01-21 20:05:57 -0500

    updated the way we get vendor location

--- smelp.js
+++ smelp.js
@@ -2,54 +2,48 @@ var TESTING = true;
 
 
 var ROOT_URL = "http://netflixrottenizer.appspot.com/yelp/";
-if (TESTING) ROOT_URL = "http://localhost:9080/yelp";
+if (TESTING) ROOT_URL = "http://localhost:11090/yelp";
+
+// Get the location by scraping the source code
+var search_zip = $('body').html().match("addressJs.preloadedZip \= '(.*)\'\;")[1];
 
 $(function() {
     var $vendorList = $('#VendorsTable');
     var user_address = $('title').text();
     user_address = user_address.slice(0, user_address.indexOf('Available'));
 
-    console.log(user_address)
+    console.log(window.frames)
 
-    var observer = new MutationObserver(function(mutation) {
-        console.log(mutation);
-    });
-    observer.observe(document, { childList: true })
-
-    
     $vendorList.on('DOMSubtreeModified', function(ok) {
-        console.log(ok)
         console.log('stuff added');
     });
     
-    $('#infscr-loading').attr('data-loading', 'false');
-
-    console.log($('#infscr-loading'))
-   
     $vendorList.find('tr').each(function() {
         var $this = $(this);
         var restaurant = $this.find('h3 .tooltip-header').text().replace(/[^\w\s]/gi, ''); // get rid of special characters
         
         var $ratingCell = $this.find('td:nth-child(4)')
 
-        var url = ROOT_URL + '?term=' + restaurant + '&location=' + user_address + '&limit=1'
+        var url = ROOT_URL + '?term=' + restaurant + '&location=' + search_zip + '&limit=1'
         $.getJSON(url).
             success(function(data) {
                 if(!data.error && data.businesses.length) {
-                    var restaurant = data.businesses[0];
-
-                    var smelp_rating = $(
-                            '<div class="smelp-rating">' +
-                                '<a href="' + restaurant.url + '" target="_blank">' + 
-                                    '<img src="' + chrome.extension.getURL('img/yelp-icon.png') + '" />' +
-                                    restaurant.rating  + 
-                                '</a>' +
-                            '</div>'
-                        );
+                    var smelp_rating = createSmelpRating(data.businesses[0])
                     $ratingCell.append(smelp_rating);    
                 }
             })
         ;
     });
 });
+
+function createSmelpRating(restaurant) {
+    return $('<div class="smelp-rating">' +
+                '<a href="' + restaurant.url + '" target="_blank">' + 
+                    '<img src="' + chrome.extension.getURL('img/yelp-icon.png') + '" />' +
+                    restaurant.rating  + 
+                '</a>' +
+            '</div>'
+        )
+    ;
+}
 
