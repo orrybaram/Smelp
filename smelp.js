c936939b9f76de7fa00de8d01b9e7c0fce98dff7
Orry Baram <orrybaram@gmail.com>
2014-01-23 21:12:45 -0500

    photos working

--- smelp.js
+++ smelp.js
@@ -51,6 +51,9 @@ $(function() {
         var restaurant_name = $('#VendorName').text();
         var restaurant_zip = $('[itemprop="postalCode"]').text();
         var url = ROOT_URL + 'yelp?term=' + restaurant_name + '&location=' + restaurant_zip + '&limit=1';
+        var photos = [];
+        var tips = [];
+
         $.getJSON(url).
             success(function(data) {
                 if(!data.error && data.businesses.length) {
@@ -69,11 +72,49 @@ $(function() {
                 $.getJSON(ROOT_URL + 'foursquare/venue?' + data).
                     success(function(venue) {
                         console.log(venue)
-                        addFoursquarePhotos(venue.response.venue.photos.groups[1].items)
+                        photos = venue.response.venue.photos.groups[1].items;
+                        addFoursquareTips(venue.response.venue.tips.groups[0].items);
+                            
                     })
                 ;
             })
         ;
+    }
+
+    // Hack for the Tabs
+    $('#RestaurantPage').on('click', '#MenuPlusTabs a', function() {
+        var $tips = $("#smelp-foursquare-tips");
+        
+        // Let the page load a bit
+        setTimeout(function() {
+            var hash = window.location.hash
+
+            if( hash === '#foursquare-tips' ) {
+                $tips.show();
+            }
+            else if ( hash === '#photos-tab') {
+                whenPhotosDoneLoading( addFoursquarePhotos );
+            } 
+            else if (hash !== '#foursquare-tips') {
+                $tips.hide();
+            } 
+            
+        }, 10)
+        
+    });
+
+
+
+
+    function whenPhotosDoneLoading(callback) {
+        if ($('#menuPlusImagesContent').children().length === 0) {
+            setTimeout(function() { 
+                whenPhotosDoneLoading(callback) 
+            }, 1000)
+            return false;
+        } else {
+            if (callback) callback(photos);
+        }
     }
 });
 
@@ -91,18 +132,37 @@ function createSmelpRating(restaurant) {
 }
 
 function addFoursquarePhotos(photos) {
-    console.log(photos)
-    var $note = '<p class="smelp-note">Additional pictures added by Smelp, provide by Foursquare</p>'
-    for (var i = 0; i < photos.length; i++) {
-        var photo = photos[i];
+    console.log('add em')
+    if(photos && $('.smelp-photos').length === 0) {
+        
+        var $note = '<p class="smelp-note">Additional pictures added by Smelp, provide by Foursquare</p>'
+        var $smelp_photos = $('<div class="smelp-photos"></div>');
+        for (var i = 0; i < photos.length; i++) {
+            var photo = photos[i];
+            var $photo = '<a href="' + photo.url + '" target="_blank"><img class="thumb smelp-photo" src="' + photo.sizes.items[1].url + '"/></a>'
+            $smelp_photos.append($photo)
+        };
+        $smelp_photos.append($note);
 
-        var $photo = '<a href="' + photo.url + '" target="_blank"><img class="thumb smelp-photo" src="' + photo.sizes.items[1].url + '"/></a>'
+        $('.photos-tab').append($smelp_photos);    
+    }
+}
 
 
-        $('.photos-tab #menuPlusImagesContent')
-            .append($photo)
-    };
-    $('.photos-tab').append($note)
 
+function addFoursquareTips(tips) {
+    var $tipsContainer = '<div id="smelp-foursquare-tips" class="foursquare-tips-tab" style="display: none;">hahaha</div>';
+    $('#MenuPlusContent').append($tipsContainer);
+    $('#MenuPlusTabs').append(
+        '<li><h2 class="tab-header">' +
+            '<a href="#foursquare-tips" data-tab="#foursquare-tips-tab">User Tips</a>' +
+        '</h2></li>');
+    
+    console.log(tips);
+    if(tips) {
+        for (var i = 0; i < tips.length; i++) {
+            var tip = tips[i];
+        };
+    }
 }
 
